9b7306732ce38fa705095dfc76099ccc566d32b5
shaawtymaker <akankshchinnu@gmail.com>
2025-04-28 18:37:42 +0530

    jwt token

--- backend/utils/jwt_util.php
+++ backend/utils/jwt_util.php
@@ -6,7 +6,7 @@ class JWTUtil {
     
     public function __construct() {
         // In a real app, you should use a strong secret key from environment variables
-        $this->secret_key = 'your_secret_key_here';
+        $this->secret_key = '86543a53d1db8eba8d5491bfb44f5dfcd30c0dc28f3b729748175ff05054794a52c55ad0e61f80a60faf124afcd1e385e2409910eb703b09a95c9398d2687797b5b6ca4826505f5665418c39d8b9b7212d25011d545332c98160ed345e6a525a0769db4dbc87fadd5b037bac98ed12ea686653f86479f822d623372f1a8d116240016298b8cd3aed8dcde0e80ca9fe0a85bd3dbdd0ae82165a51a273373cd46f3c239717f5fcb21fc4a166ed24ad6c8590e304309dbc04ac1d70e6e3200bc3c31ae306fd964de6886d533126dd053ab8f9d02c508ec964a5094c5225fbd6f3bde5a54b961d1923a16c06afbd3b1dc9b80d7fd81e4737494863abd5647ed98016';
         $this->token_expiry = 86400; // 24 hours
     }
     
